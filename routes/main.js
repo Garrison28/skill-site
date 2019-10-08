531351a38e024580394310f0cd0341d25264bda1
Garrison28 <ghighsmith28@gmail.com>
2019-10-07 19:54:19 -0700

    getting a this.stack.push(layer) error

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router;
 
-router.get('/', function(req, res) {
-    res.render()
-})
+router.get('/skills', function(req,res) {
+    res.render('skills', {name: 'Garrison Highsith', 
+                        skills: ['html', 'CSS', 'Javascript', 'Terminal/GIT', 'Playing the Saxaphone']})
+});
+
+
+router.get('/contact', function(req, res) {
+    res.render('contact', {contacts: [
+        {cell: '850-901-7232'},
+        {email: 'ghighsmith28@gmail.com'},
+        {github: 'github.com/Garrison28'}
+    ]});
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 
-app.get('/', function(req, res) {
-    res.send('You hit the home route');
-});
-
 app.use('/', require('./routes/main'));
 
-app.listen(3000, function() {
+app.get('/', function (req, res) {
+    res.render('home')
+})
+
+app.listen(3000, function () {
     console.log('Server running')
 })
