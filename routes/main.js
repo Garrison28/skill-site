8145ab1482975565b9cd980ac750207790aa55f3
Garrison28 <ghighsmith28@gmail.com>
2019-10-08 08:18:31 -0700

    working, could not get my routes folder to work

--- routes/main.js
+++ routes/main.js
@@ -1,19 +1,6 @@
-const express = require('express');
-const router = express.Router;
-
-router.get('/skills', function(req,res) {
-    res.render('skills', {name: 'Garrison Highsith', 
-                        skills: ['html', 'CSS', 'Javascript', 'Terminal/GIT', 'Playing the Saxaphone']})
-});
+// const express = require('express');
+// const router = express.Router;
 
 
-router.get('/contact', function(req, res) {
-    res.render('contact', {contacts: [
-        {cell: '850-901-7232'},
-        {email: 'ghighsmith28@gmail.com'},
-        {github: 'github.com/Garrison28'}
-    ]});
-});
 
-
-module.exports = router;
+// module.exports = router;
--- server.js
+++ server.js
@@ -6,11 +6,24 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 
-app.use('/', require('./routes/main'));
+// app.use('/', require('./routes/main'));
 
 app.get('/', function (req, res) {
-    res.render('home')
+    res.sendFile(__dirname + '/views/home.html')
 })
+
+app.get('/skills', function(req,res) {
+    res.render('skills', {name: 'Garrison Highsith', 
+                        skills: ['html', 'CSS', 'Javascript', 'Terminal/GIT', 'Playing the Saxaphone']})
+});
+
+app.get('/contact', function(req, res) {
+    res.render('contact', {contacts: [
+        {cell: '850-901-7232'},
+        {email: 'ghighsmith28@gmail.com'},
+        {github: 'github.com/Garrison28'}
+    ]});
+});
 
 app.listen(3000, function () {
     console.log('Server running')
