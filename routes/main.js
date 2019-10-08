d55eb548bb637819c6e551a4734f6518476dbaf6
Garrison28 <ghighsmith28@gmail.com>
2019-10-08 08:27:33 -0700

    added my router file

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,21 @@
 // const express = require('express');
 // const router = express.Router;
 
+// router.get('/', function (req, res) {
+//     res.sendFile(__dirname + '/views/home.html')
+// })
 
+// router.get('/skills', function(req,res) {
+//     res.render('skills', {name: 'Garrison Highsith', 
+//                         skills: ['html', 'CSS', 'Javascript', 'Terminal/GIT', 'Playing the Saxaphone']})
+// });
+
+// router.get('/contact', function(req, res) {
+//     res.render('contact', {contacts: [
+//         {cell: '850-901-7232'},
+//         {email: 'ghighsmith28@gmail.com'},
+//         {github: 'github.com/Garrison28'}
+//     ]});
+// });
 
 // module.exports = router;
